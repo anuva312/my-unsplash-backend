c91915436d3fc61650f2b01cfa6db5ffbd5f9025
Anusree V A <anusreeva93@gmail.com>
2023-09-16 18:45:07 +0530

    moved listen method into the connect callback

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-mongoose.connect(DB).then(() => console.log("Connection to DB Successful 🤩"));
-
-// Listen to the client
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+mongoose.connect(DB).then(() => {
+  console.log("Connection to DB Successful 🤩");
+  // Listen to the client
+  app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
 });
